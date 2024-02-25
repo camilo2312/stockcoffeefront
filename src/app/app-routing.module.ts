07ad985118d31006fb5d95fbdc7c908f57b24a98
camilo2312 <42982644+camilo2312@users.noreply.github.com>
2024-02-24 21:36:57 -0500

    Agrego login con estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('src/app/modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  { path: '', redirectTo: 'pages/home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register-user', component: RegisterUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
